3a0ed901ba2c651c1901a258d53dcd1fbd0d7973
Guilherme Guimarães <g-guimaraes@live.com>
2017-05-20 00:36:20 -0300

    Primeiro protótipo da tela de exercícios

---
+++ app_front/pages/exercicio.js
@@ -0,0 +1,19 @@
+export default () => (
+  <div>
+    <h1 style={{ textAlign: "center" }}>Qual é o quadrado?</h1>
+    <div>
+      <img
+        style={{ width: "33%" }}
+        src="https://oblogmenoslidodomundo.files.wordpress.com/2014/10/quadrado.jpg"
+      />
+      <img
+        style={{ width: "33%" }}
+        src="http://3.bp.blogspot.com/_N8rtcf8YfEo/R0YKb--LZPI/AAAAAAAAARI/jwCcX95MCI0/s320/Estado+do+Triangulo.JPG"
+      />
+      <img
+        style={{ width: "33%" }}
+        src="http://icon-icons.com/icons2/321/PNG/512/Circle_34541.png"
+      />
+    </div>
+  </div>
+)
