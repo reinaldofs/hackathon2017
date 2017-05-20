ed5263cae054506fac29fec8f0ebb55811119039
fernandobd42 <fernandolucasbd@gmail.com>
2017-05-20 07:44:46 +0000

    adicionando Menu e ajustando Questao e Ajuda

--- app_front/pages/exercicio.js
+++ app_front/pages/exercicio.js
@@ -2,6 +2,7 @@ import { Component } from "react"
 import Questao from "../components/Questao.js"
 import Acerto from "../components/Acerto.js"
 import Erro from "../components/Erro.js"
+import Menu from "../components/Menu.js"
 
 const QUESTAO = "QUESTAO"
 const ERRO = "ERRO"
@@ -34,11 +35,11 @@ export default class Exercicio extends Component {
             height: 100;
             font-family: Courier New;
             color: #212121;
-            text-shadow: 2px 2px 5px white;
             font-size: 26px;
             background: #f5f5f5;
           }    
       `}</style>
+        <Menu title="Exercicio 1"/>
         <Questao
           onAnswer={isRight =>
             this.setState({ questionState: isRight ? ACERTO : ERRO })}
