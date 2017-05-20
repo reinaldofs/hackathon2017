3f763e0e615afe0309fdf641e12146495902b8fd
fernandobd42 <fernandolucasbd@gmail.com>
2017-05-20 13:48:44 +0000

    adicionando imagem e audio aos acertos

--- app_front/components/Acerto.js
+++ app_front/components/Acerto.js
@@ -1,3 +1,35 @@
+import Router from "next/router"
+
 export default () => (
-  <h1 style={{ textAlign: "center" }}>Parabéns! Você acertou!</h1>
+  <div>
+    <style global jsx>{`
+             @keyframes visible {
+              from {
+                  left: 60%;
+              }
+
+              50% {
+                  left: 10%;
+              }
+
+              to {
+                  left: 60%;
+              }
+           }
+
+            .acerto {
+              position: fixed;
+              top: 500px;
+              left: 60%;
+              animation-duration: 3.5s;
+              animation-name: visible;
+
+            }
+      `}</style>
+    <div id="img" className="acerto">
+      <img src="/static/imagens-coisas/parabens.png"/>
+      <audio src="/static/audios/acerto.mp3" autoPlay={true} />
+    </div>
+  </div>
+
 )
