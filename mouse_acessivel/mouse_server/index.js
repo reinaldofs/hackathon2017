7afc119713e719b85ccffbace1a136683d72d6ab
Reinaldo Fernandes Santos <reinaldofernandessantos@MacBook-Pro-de-Reinaldo.local>
2017-05-20 00:21:54 -0300

    Movimento do mouse e criacao de endpoint para retornar a direcao do joystick

--- mouse_acessivel/mouse_server/index.js
+++ mouse_acessivel/mouse_server/index.js
@@ -2,6 +2,10 @@
 var robot = require("robotjs");
 // Auxiliar para leitura da porta serial
 var SerialPort = require('serialport');
+// Servidor para fornecer os dados para o frontend
+var express = require('express'),
+    app = express();
+
 
 // Conecta na porta serial para receber as coordenadas do joystick 
 var port = new SerialPort('/dev/cu.usbmodem14411', {
@@ -11,8 +15,63 @@ var port = new SerialPort('/dev/cu.usbmodem14411', {
 // Define o listener para leitura da porta serial
 port.on('data', loop_arduino);
 
+// Variaveis para o controle da posicao do ponteiro
+var incX = 0; // Incremento X
+var incY = 0; // Incremento Y
+var sensibilidade = 2; // Sensibilidade do ponteiro
+var tempoMovimento = 1; // Tempo para movimentacao do cursor
+
+// Retorno para a aplicacao frontend
+var retorno = {direcao:{x:0,y:0}, botoes:{click:false}};
+
 // Funcao que trata as leituras
 function loop_arduino(data){
 	var values = data.split(',');
-	console.log('Arduino: ', values);
+	var joystick = {x:Number(values[1]), y:Number(values[0])};
+	
+	// Testa se esta indo para os lados
+	if (joystick.x<500){
+		incX = -sensibilidade;
+	}else if (joystick.x>515){
+		incX = sensibilidade;
+	}else{
+		incX = 0;
+	}
+
+	if (joystick.y<500){
+		incY = sensibilidade;
+	}else if (joystick.y>525){
+		incY = -sensibilidade;
+	}else{
+		incY = 0;
+	}
+
+	// o retorno para o frontend sera somente a direcao
+	retorno.direcao.x = incX > 0 ? 1 : incX < 0 ? -1 : 0;
+	retorno.direcao.y = incY > 0 ? 1 : incY < 0 ? -1 : 0;
 }
+
+
+// Metodo que realiza a movimentacao do ponteiro
+function mouseMove(){
+	// Obtem a posicao do mouse
+	var mouse = robot.getMousePos();
+    
+    if (incX!=0||incY!=0){
+		// Realiza o movimento
+		mouse.x += incX;
+		mouse.y += incY;
+		robot.moveMouse(mouse.x, mouse.y);
+	}
+}
+
+// Inicia a escuta de movimentacao do mouse
+setInterval(mouseMove, tempoMovimento);
+
+// inicia o servidor para fornecer para o
+var server = app.listen(1234);
+
+// Fornece a direcao do joystick para o frontend quando requisitado
+app.get('/joystick', function(req, res){
+    res.send(retorno);
+});
