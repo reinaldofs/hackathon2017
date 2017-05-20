51d227e10fc9e1a1b722ce2e3d5e2af15c67777c
fernandobd42 <fernandolucasbd@gmail.com>
2017-05-20 06:05:00 +0000

    adicionando stylo as paginas

--- app_front/components/Ajuda.js
+++ app_front/components/Ajuda.js
@@ -1,16 +1,42 @@
 export default () => (
     <div>
         <style jsx>{` 
+            @keyframes grow {
+            from {
+              transform: scale(1, 1);
+            }
+
+            to {
+              transform: scale(1.5, 1.5);
+            }
+        }
+        
+            @keyframes decrease {
+            from {
+              transform: scale(1.5, 1.5);
+            }
+
+            to {
+             transform: scale(1, 1);
+            }
+        }
+
             .img-faq {
                 display: block;
                 position: absolute;
-                margin-left: 95%;
+                margin-top: 10px;
+                margin-left: 94%;
                 width: 4%;
                 height: 4%;
+                transform: scale(1, 1);
+                animation-duration: 0.5s;
+                animation-name: decrease;
             }
 
             .img-faq:hover {
                 transform: scale(1.5, 1.5);
+                animation-duration: 0.5s;
+                animation-name: grow
             }
             `}
         </style>
